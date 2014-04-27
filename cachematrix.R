126e70066577f40ba0f2284ff4caa19e0208cb14
Dorota Milewska <dkmilewska@gmail.com>
2014-04-27 10:21:43 -0400

    Implement cacheMatrix module.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions invert a matrix and cache it. If the inverted matrix is in cache, 
+## it isn't calculated again, but read from cache.
 
-## Write a short comment describing this function
+## Returns the object on which the inversion is performed. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinversion <- function(solve) m <<- solve
+  getinversion <- function() m
+  list(set = set, get = get,
+       setinversion = setinversion,
+       getinversion = getinversion)
 }
 
 
-## Write a short comment describing this function
+## Checks if the inversion has already been calculated and reads it; if not,
+## makes the calculation. The argument "x" is not a matrix, but the result of 
+## function "makeCacheMatrix"
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinversion()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinversion(m)
+  m
 }
